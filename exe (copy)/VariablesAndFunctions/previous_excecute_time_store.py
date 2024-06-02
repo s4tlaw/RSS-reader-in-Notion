f524c14e4173a7ff631f26c3a826ace4704c913a
s4tlaw <166223202+s4tlaw@users.noreply.github.com>
2024-06-02 21:45:15 +0800

    Delete exe (copy) directory

--- exe (copy)/VariablesAndFunctions/previous_excecute_time_store.py
+++
@@ -1,1 +0,0 @@
-previous_excecute_time = "Tue, 16 Apr 2024 14:06:01 +0000"
--- exe (copy)/environment.py
+++
@@ -1,4 +0,0 @@
-NOTION_TOKEN = "secret_J<yourtoken>L"
-DATABASE_ID_WD = "1<yourdbid>5"
-DATABASE_ID_HK = "0<yourdbid>5"
-DATABASE_ID_YT = "0<yourdbid>0"
